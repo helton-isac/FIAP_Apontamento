9261768cc22c23e0dca5024016979f23c5aa5717
Helton Isac <helton.isac@gmail.com>
2020-08-19 19:29:22 -0300

    Learning Hash

--- src/login/login.js
+++ src/login/login.js
@@ -2,12 +2,11 @@ import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 
 function Login(props) {
-    console.log("debug location: " + JSON.stringify(props.location));
-    console.log("debug search: " + JSON.stringify(props.location.search));
-    console.log("debug hash: " + JSON.stringify(props.location.hash));
-    alert("wait");
-    const urlParams = new URLSearchParams(props.location.search);
-    const id_token = urlParams.get('id_token');
+    // console.log("debug location: " + JSON.stringify(props.location.hash));
+    // console.log("debug search: " + JSON.stringify(props.location.search));
+    // console.log("debug hash: " + JSON.stringify(props.location.hash));
+    // const urlParams = new URLSearchParams(props.location.hash);
+    const id_token = props.location.hash;//urlParams.get('id_token');
 
     if (!id_token) {
        window.location.assign('https://apontamento.auth.us-east-1.amazoncognito.com/login?client_id=2bk2he7s7lgmaovtrtc17bat9t&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://master.d1t6bh9zd2tcsz.amplifyapp.com/');
