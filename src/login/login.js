d5abadcf86e457452c54b961f4539a423d4e7e56
Helton Isac <helton.isac@gmail.com>
2020-08-18 22:35:49 -0300

    debugging in production

--- src/login/login.js
+++ src/login/login.js
@@ -3,6 +3,7 @@ import { Redirect } from 'react-router-dom';
 
 function Login() {
     const urlParams = new URLSearchParams(window.location.search);
+    alert(urlParams);
     const id_token = urlParams.get('id_token');
     if (!id_token) {
         window.location.assign('https://apontamento.auth.us-east-1.amazoncognito.com/login?client_id=2bk2he7s7lgmaovtrtc17bat9t&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://master.d1t6bh9zd2tcsz.amplifyapp.com/');
