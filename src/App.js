9de8f78dbd88723ce41077807feb6fa63a6d671f
Helton Isac <helton.isac@gmail.com>
2020-08-19 00:00:21 -0300

    last attempt

--- src/App.js
+++ src/App.js
@@ -5,48 +5,11 @@ import Report from './report/report';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import NotFoundPage from './404/404';
 
-const fakeAuth = {
-  isAuthenticated: false,
-  authenticate(cb) {
-    fakeAuth.isAuthenticated = true;
-    setTimeout(cb, 100); // fake async
-  },
-  signout(cb) {
-    fakeAuth.isAuthenticated = false;
-    setTimeout(cb, 100);
-  }
-};
-
-// A wrapper for <Route> that redirects to the login
-// screen if you're not yet authenticated.
-function PrivateRoute({ children, ...rest }) {
-  return (
-    <Route
-      {...rest}
-      render={({ location }) =>
-        fakeAuth.isAuthenticated ? (
-          children
-        ) : (
-            <Redirect
-              to={{
-                pathname: "/",
-                state: { from: location }
-              }}
-            />
-          )
-      }
-    />
-  );
-}
-
-
-
 function App() {
-
   return (
     <Router>
       <Switch>
-        <Route exact path="/" render={() => <Login search={window.location.search} />} />
+        <Route exact path="/" component={Login} />
         <Route exact path="/404" component={NotFoundPage} />
         <Route exact path="/report" component={Report} />
         <Redirect to="/404"></Redirect>
--- src/login/login.js
+++ src/login/login.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 
-function Login() {
-    const urlParams = new URLSearchParams(window.location.search);
+function Login(props) {
+    const urlParams = new URLSearchParams(props.location.search);
     const id_token = urlParams.get('id_token');
-    console.log(urlParams.toString());
 
     if (!id_token) {
        window.location.assign('https://apontamento.auth.us-east-1.amazoncognito.com/login?client_id=2bk2he7s7lgmaovtrtc17bat9t&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://master.d1t6bh9zd2tcsz.amplifyapp.com/');
