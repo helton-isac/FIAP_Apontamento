3b14f076704c7f26cb3491a616b43072a6554493
Helton Isac <helton.isac@gmail.com>
2020-08-19 21:40:34 -0300

    correctly getting the id_token

--- src/login/login.js
+++ src/login/login.js
@@ -1,21 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Redirect } from 'react-router-dom';
 
 function Login(props) {
-    // console.log("debug location: " + JSON.stringify(props.location.hash));
-    // console.log("debug search: " + JSON.stringify(props.location.search));
-    // console.log("debug hash: " + JSON.stringify(props.location.hash));
-    // const urlParams = new URLSearchParams(props.location.hash);
-    const id_token = props.location.hash;//urlParams.get('id_token');
+    const hashParams = props.location.hash.split('&');
+    const id_token = extractParam(hashParams, 'id_token');
 
     if (!id_token) {
        window.location.assign('https://apontamento.auth.us-east-1.amazoncognito.com/login?client_id=2bk2he7s7lgmaovtrtc17bat9t&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://master.d1t6bh9zd2tcsz.amplifyapp.com/');
+    } else {
+
     }
 
     return (
         <Redirect to="/report" />
     );
 }
+
+function extractParam(params,param){
+    for(var i = 0; i < params.length; i++){
+        if(params[i].indexOf(param) > 0) {
+            return params[i].replace('#','').replace(`${param}=`,'');
+        }
+    }
+    return null;
+}
+
 
 export default Login;
 
