b9bcf3b72bfa1f3dfd8998561c014cb04b09e4ee
Helton Isac <helton.isac@gmail.com>
2020-08-20 23:00:28 -0300

    Fixing reference to login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Login from './login/login';
 import Report from './report/report';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import NotFoundPage from './404/404';
