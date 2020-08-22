1f9ae2438f1907e42d0b222e8259e7e7081fd4cd
Helton Isac <helton.isac@gmail.com>
2020-08-21 21:04:15 -0300

    Removing the hash from url

--- src/menu/menu.jsx
+++ src/menu/menu.jsx
@@ -1,31 +1,24 @@
 import React, { useState } from 'react';
-import { ReactComponent as Logo } from './../logo.svg';
 import LogoWithName from '../components/logoWithName';
 import Menus from '../components/menus';
 import TimeTracking from '../components/timeTrackingPanel';
 
 function Menu(props) {
 
-
     const hashParams = props.location.hash.split('&');
     const sign_in = 'Entrar';
     const sign_out = 'Sair';
     const idToken = extractParam(hashParams, 'id_token');
-    const expiresIn = extractParam(hashParams, 'expires_in');
-    const tokenType = extractParam(hashParams, 'token_type');
+    // TODO: It is necessary to revalidate the token sometime.
+    // const expiresIn = extractParam(hashParams, 'expires_in');
+    // const tokenType = extractParam(hashParams, 'token_type');
 
-    function parseJwt(token) {
-        if (!token) return null;
-        var base64Url = token.split('.')[1];
-        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
-            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        }).join(''));
-
-        return JSON.parse(JSON.stringify(jsonPayload));
-    };
     const [user, setUser] = useState(JSON.parse(parseJwt(idToken)));
     const [showTimeTracking, setShowTimeTracking] = useState(false);
+
+    if (user) {
+        window.history.replaceState(null, null, ' ');
+    }
 
     const login = () => {
         window.location.assign('https://apontamento.auth.us-east-1.amazoncognito.com/login?client_id=2bk2he7s7lgmaovtrtc17bat9t&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://master.d1t6bh9zd2tcsz.amplifyapp.com/');
@@ -60,6 +53,21 @@ function extractParam(params, param) {
     }
     return null;
 }
+
+function parseJwt(token) {
+    if (!token) return null;
+    try{
+        var base64Url = token.split('.')[1];
+        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+    
+        return JSON.parse(JSON.stringify(jsonPayload));
+    } catch (e) {
+        return null;
+    }
+};
 
 const styles = {
     page: {
